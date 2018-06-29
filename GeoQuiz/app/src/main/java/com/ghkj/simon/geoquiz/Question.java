e481d7909b03de999e70381afa440ab93b08322c
simon <who8023me@hotmail.com>
2018-06-29 17:01:26 +0800

    add question class

---
+++ GeoQuiz/app/src/main/java/com/ghkj/simon/geoquiz/Question.java
@@ -0,0 +1,27 @@
+package com.ghkj.simon.geoquiz;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+    
+    public Question(int textResId,boolean answerTrue)
+    {
+        mTextResId=textResId;
+        mAnswerTrue=answerTrue;
+    }
+}
